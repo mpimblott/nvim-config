ca2d8428c4a172795ef435aad03454a858c95414
Matthew Pimblott <matthew.pimblott@kaplan.co.uk>
2026-02-10 12:01:57 +0000

    Add telescope file browser

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,16 +25,6 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
-		-- import your plugins
-		{
-			"nvim-telescope/telescope.nvim",
-			version = "*",
-			dependencies = {
-				"nvim-lua/plenary.nvim",
-				-- optional but recommended
-				{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-			},
-		},
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
@@ -57,12 +47,3 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.wo[0][0].foldmethod = "expr"
 	end,
 })
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "Go to definition" })
-vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "Find references" })
-vim.keymap.set("n", "gi", builtin.lsp_implementations, { desc = "Go to Implementations" })
