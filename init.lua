5cd03c50ba82d2819a4d8462dc809a359749abf3
Matthew Pimblott <30576480+mpimblott@users.noreply.github.com>
2026-02-10 22:22:27 +0000

    Formatting

--- init.lua
+++ init.lua
@@ -2,18 +2,18 @@ vim.g.mapleader = " " -- Use Space as your leader key
 vim.g.maplocalleader = " "
 
 -- SETTINGS: General behavior
-vim.opt.number = true -- Show line numbers
+vim.opt.number = true         -- Show line numbers
 vim.opt.relativenumber = true -- Relative line numbers (great for jumping)
-vim.opt.mouse = "a" -- Enable mouse support
+vim.opt.mouse = "a"           -- Enable mouse support
 -- Indentation
-vim.opt.tabstop = 4 -- Tabs look like 4 spaces
-vim.opt.shiftwidth = 4 -- Auto-indent uses 4 spaces
-vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.tabstop = 4           -- Tabs look like 4 spaces
+vim.opt.shiftwidth = 4        -- Auto-indent uses 4 spaces
+vim.opt.expandtab = true      -- Convert tabs to spaces
 -- Search
-vim.opt.ignorecase = true -- Ignore case in search patterns
-vim.opt.smartcase = true -- ...unless there's a capital letter
+vim.opt.ignorecase = true     -- Ignore case in search patterns
+vim.opt.smartcase = true      -- ...unless there's a capital letter
 -- UI
-vim.opt.termguicolors = true -- Better colors (standard in modern terminals)
+vim.opt.termguicolors = true  -- Better colors (standard in modern terminals)
 
 -- Folding
 vim.opt.foldminlines = 30
@@ -23,18 +23,18 @@ vim.o.autoread = true
 
 -- Autocommand to run 'checktime' on specific events
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
-  command = "if mode() != 'c' | checktime | endif",
-  pattern = { "*" },
+    command = "if mode() != 'c' | checktime | endif",
+    pattern = { "*" },
 })
 vim.api.nvim_create_autocmd("FileChangedShellPost", {
-  command = "echomsg 'File changed on disk. Buffer reloaded.'",
-  pattern = { "*" },
+    command = "echomsg 'File changed on disk. Buffer reloaded.'",
+    pattern = { "*" },
 })
 
 -- KEYMAPS: Quality of life
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Space+p+v opens the file explorer
-vim.keymap.set("n", "<C-d>", "<C-d>zz") -- snap cursor to centre of screen when navigating down
-vim.keymap.set("n", "<C-u>", "<C-u>zz") -- snap cursor to centre of screen when navigating up
+vim.keymap.set("n", "<C-d>", "<C-d>zz")       -- snap cursor to centre of screen when navigating down
+vim.keymap.set("n", "<C-u>", "<C-u>zz")       -- snap cursor to centre of screen when navigating up
 
 -- Resize with Ctrl + Arrow Keys
 vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
@@ -56,14 +56,20 @@ vim.opt.updatetime = 1000
 
 --  Create the AutoSave command
 vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-	pattern = "*",
-	callback = function()
-		-- Only save if the buffer has a name and is modified
-		if vim.fn.getbufinfo("%")[1].name ~= "" and vim.bo.modified then
-			vim.cmd("silent! write")
-			print("AutoSaved")
-		end
-	end,
+    pattern = "*",
+    callback = function()
+        -- Only save if the buffer has a name and is modified
+        if vim.fn.getbufinfo("%")[1].name ~= "" and vim.bo.modified then
+            vim.cmd("silent! write")
+            print("AutoSaved")
+        end
+    end,
 })
 
 vim.env.PATH = vim.env.PATH .. ":/home/linuxbrew/.linuxbrew/bin"
+
+vim.filetype.add({
+    extension = {
+        yml = "yaml",
+    },
+})
--- lua/plugins/telescopeFileBrowser.lua
+++ lua/plugins/telescopeFileBrowser.lua
@@ -1,20 +1,20 @@
 return {
-	"nvim-telescope/telescope-file-browser.nvim",
-	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-	config = function()
-		local telescope = require("telescope")
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+        local telescope = require("telescope")
 
-		telescope.setup({
-			extensions = {
-				file_browser = {
-					-- theme = "ivy", (optional: change the look)
-					-- hijack_netrw = true,
-				},
-			},
-		})
+        telescope.setup({
+            extensions = {
+                file_browser = {
+                    -- theme = "ivy", (optional: change the look)
+                    hijack_netrw = true,
+                },
+            },
+        })
 
-		telescope.load_extension("file_browser")
+        telescope.load_extension("file_browser")
 
-		vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
-	end,
+        vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+    end,
 }
