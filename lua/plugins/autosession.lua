20757ff7bb643d0fbb74b39786f0f9424f91a9f0
Matthew Pimblott <30576480+mpimblott@users.noreply.github.com>
2026-02-14 22:14:44 +0000

    Version updates, add autosession commands, add markdown treesitter requirement

--- lua/plugins/autosession.lua
+++ lua/plugins/autosession.lua
@@ -1,14 +1,19 @@
 return {
-	"rmagatti/auto-session",
-	lazy = false,
-
-	---enables autocomplete for opts
-	---@module "auto-session"
-	---@type AutoSession.Config
-	opts = {
-        auto_restore_last_session= true,
-        session_lens= {
+    "rmagatti/auto-session",
+    lazy = false,
+    keys = {
+        -- Will use Telescope if installed or a vim.ui.select picker otherwise
+        { "<leader>wr", "<cmd>AutoSession search<CR>", desc = "Session search" },
+        { "<leader>ws", "<cmd>AutoSession save<CR>",   desc = "Save session" },
+        { "<leader>wa", "<cmd>AutoSession toggle<CR>", desc = "Toggle autosave" },
+    },
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+        auto_restore_last_session = true,
+        session_lens = {
             picker = "telescope"
         }
-	},
+    },
 }
