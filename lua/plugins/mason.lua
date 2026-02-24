2a8563e5dc593dc5b411ee77b3b53916d38fda14
Matthew Pimblott <30576480+mpimblott@users.noreply.github.com>
2026-02-24 19:59:57 +0000

    Fix typescript language server package name

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,6 +5,6 @@ return {
 		"neovim/nvim-lspconfig",
 	},
 	opts = {
-        ensure_installed = {"terraformls", "sqls", "basedpyright", "clangd", "lua_ls", "typescript-language-server", "denols"}
+        ensure_installed = {"terraformls", "sqls", "basedpyright", "clangd", "lua_ls", "ts_ls", "denols"}
     }
 }
