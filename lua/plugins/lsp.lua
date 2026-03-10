5b5a9cb76d9e261157d176a1ad633f742bb6ec52
Matthew Pimblott <30576480+mpimblott@users.noreply.github.com>
2026-03-10 22:14:26 +0000

    Deno and sql lsp configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,12 +7,22 @@ return {
     config = function()
         -- For sqls create a file: ~/.config/sqls/config.yml
         -- Use the new Neovim 0.11+ API to avoid deprecation warnings
-        
+
         -- Configure sqls with filetype restrictions and root markers
-        vim.lsp.config('sqls', {
-            filetypes = { 'sql', 'mysql', 'plsql' },
-            root_markers = { ".sqls.yml", ".git" },
+        vim.lsp.config('postgres_lsp', {
+            filetypes = { 'sql', 'psql' },
+            root_markers = { ".git" },
+            settings = {
+                postgres_lsp = {
+                    connectionString = "postgresql://postgres:postgres@127.0.0.1:54322/postgres"
+                }
+            }
         })
+        -- vim.lsp.config('sqls', {
+        -- filetypes = { 'sql', 'mysql', 'plsql' },
+        -- root_markers = { ".sqls.yml", ".git" },
+        -- cmd = {"sqls", "-config", "~/.config/sqls/config.yml"}
+        -- })
 
         -- Configure denols and ts_ls root markers
         vim.lsp.config('denols', {
@@ -29,6 +39,36 @@ return {
         vim.lsp.enable('clangd')
         vim.lsp.enable('lua_ls')
         vim.lsp.enable('sqls')
+
+        vim.lsp.config('denols', {
+            root_markers = { 'deno.json', 'deno.jsonc' },
+            settings = {
+                deno = {
+                    enable = true,
+                    -- Deno resolves this relative to the matched root_marker directory!
+                    config = "./deno.json",
+                    suggest = {
+                        imports = {
+                            hosts = {
+                                ["https://deno.land"] = true
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        -- 2. Configure ts_ls to IGNORE Deno directories
+        -- vim.lsp.config('ts_ls', {
+        -- root_dir = function(bufnr)
+        -- -- If we are in a Deno project, return nil so ts_ls backs off
+        -- if vim.fs.root(bufnr, { 'deno.json', 'deno.jsonc' }) then
+        -- return nil
+        -- end
+        -- -- Otherwise, attach to normal Node/TS projects
+        -- return vim.fs.root(bufnr, { 'package.json', 'tsconfig.json', '.git' })
+        -- end,
+        -- })
 
         -- Conditional TypeScript/Deno setup
         local is_deno = vim.fs.root(0, { "deno.json", "deno.jsonc" }) ~= nil
